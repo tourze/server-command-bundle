f4db36cb9a14eaff6b3456eb2ac7531cde3bc19a
github-ci <you@example.com>
2025-06-19 23:51:50 +0800

    update

--- src/ServerCommandBundle.php
+++ src/ServerCommandBundle.php
@@ -2,17 +2,20 @@
 
 namespace ServerCommandBundle;
 
+use ServerNodeBundle\ServerNodeBundle;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\RoutingAutoLoaderBundle\RoutingAutoLoaderBundle;
+use Tourze\ScheduleEntityCleanBundle\ScheduleEntityCleanBundle;
 
 class ServerCommandBundle extends Bundle implements BundleDependencyInterface
 {
     public static function getBundleDependencies(): array
     {
         return [
-            \Tourze\RoutingAutoLoaderBundle\RoutingAutoLoaderBundle::class => ['all' => true],
-            \Tourze\ScheduleEntityCleanBundle\ScheduleEntityCleanBundle::class => ['all' => true],
-            \ServerNodeBundle\ServerNodeBundle::class => ['all' => true],
+            RoutingAutoLoaderBundle::class => ['all' => true],
+            ScheduleEntityCleanBundle::class => ['all' => true],
+            ServerNodeBundle::class => ['all' => true],
         ];
     }
 }
